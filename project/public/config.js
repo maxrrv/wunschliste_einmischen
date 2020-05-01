986714f8aa18df3ee6e8075c005d61fd69127393
maximilian.ungar <maximilian.ungar@klarna.com>
2020-05-02 01:34:07 +0200

    Populate config with real data

--- project/public/config.js
+++ project/public/config.js
@@ -1,26 +1,77 @@
 window.websiteContent = {
   navigation: {
     home_link: "https://podcaste97de5.podigee.io/",
-    home_image: "https://is3-ssl.mzstatic.com/image/thumb/Podcasts123/v4/c2/39/26/c23926e0-2c18-b3b8-4991-6d4b4b0aa129/mza_3936908740296680901.jpg/1200x630wp.png",
+    home_image: "./images/einmischen_logo_square.png",
     title: "Jennys Wunschliste",
     paypal_link: "https://www.paypal.me/einmischen",
     twitter_link: "https://twitter.com/JennyGnther"
   },
   hero: {
-    text: "Ich liebe Bücher und deswegen freut mich jedes Geschenk meiner Hörer in diese Richtung. Vermutlich wird es dann auch mal Thema im Podcast. Wer mir vernünftige Technik zum wünschen empfehlen kann...die kommt dann hier auch rein. LG Eure Jenny"
+    text: "Ich liebe Bücher und deswegen freut mich jedes Geschenk meiner Hörer in diese Richtung. Vermutlich wird es dann auch mal Thema im Podcast. Wer mir vernünftige Technik zum wünschen empfehlen kann... die kommt dann hier auch rein. LG Eure Jenny"
   },
   wishlist: [
     {
-      image: "./images/vouchers-gift-card.jpg",
-      title: "Title",
-      subtext: "Subtext",
-      fulfilled: true
+      icon: "GiftCard",
+      title: "Gift Card",
+      subtitle: "",
+      description: "Ich brauch immer wieder was von diversen Shops im Internet. Deshalb hilfst du mir mit einer kleinen Spende wie dieser Gift card wirklich sehr :)",
+      fulfilled: false
     },
     {
-      image: "./images/vouchers-gift-card.jpg",
-      title: "Title",
-      subtext: "Subtext",
+      icon: "Book",
+      title: "The Great Influenza: The Story of the Deadliest Pandemic in History",
+      subtitle: "by John M. Barry (Paperback)",
+      description: "At the height of WWI, history’s most lethal influenza virus erupted in an army camp in Kansas, moved east with American troops, then exploded, killing as many as 100 million people worldwide. It killed more people in twenty-four months than AIDS killed in twenty-four years, more in a year than the Black Death killed in a century. But this was not the Middle Ages, and 1918 marked the first collision of science and epidemic disease. Magisterial in its breadth of perspective and depth of research and now revised to reflect the growing danger of the avian flu, The Great Influenza is ultimately a tale of triumph amid tragedy, which provides us with a precise and sobering model as we confront the epidemics looming on our own horizon. John M. Barry has written a new afterword for this edition that brings us up to speed on the terrible threat of the avian flu and suggest ways in which we might head off another flu pandemic.",
       fulfilled: false
-    }
+    },
+    {
+      icon: "Book",
+      title: "Very, Very, Very Dreadful: The Influenza Pandemic of 1918",
+      subtitle: "Albert Marrin (Autor), Jim Frangione (Erzähler), Listening Library (Verlag)",
+      description: "In spring of 1918, World War I was underway, and troops at Fort Riley, Kansas, found themselves felled by influenza. By the summer of 1918, the second wave struck as a highly contagious and lethal epidemic and within weeks exploded into a pandemic, an illness that travels rapidly from one continent to another. It would impact the course of the war, and kill many millions more soldiers than warfare itself.",
+      fulfilled: false
+    },
+    {
+      icon: "Book",
+      title: "Simulative Demokratie: Neue Politik nach der postdemokratischen Wende",
+      subtitle: "by Ingolfur Bluehdorn",
+      description: " Sind Proteste wie die der sogenannten »Wutbürger« oder internationale Bewegungen wie Occupy Wall Street ein weiterer Beleg für die Krise der Demokratie? Oder doch eher für ihre Lebendigkeit? Ist »Krise« überhaupt das richtige Wort? Immerhin bezeichnet der Begriff einen vorübergehenden Zustand, aber über Politikverdrossenheit oder gar »Postdemokratie« wird nun schon seit Jahren diskutiert. Ingolfur Blühdorn schlägt eine andere Lesart vor: Wir erleben, so seine These, einen schleichenden Formwandel des Politischen, den Übergang zur »simulativen Demokratie«, in der demokratische Werte, demokratische Verfahren, ja sogar die Idee des demokratischen Souveräns selbst sich gewissermaßen überlebt haben, gleichzeitig aber mehr öffentliche Zustimmung finden denn je - und deshalb als »Simulation« sorgfältig kultiviert werden.",
+      fulfilled: false
+    },
+    {
+      icon: "Book",
+      title: "Ein Mann seiner Klasse",
+      subtitle: "by Christian Baron (Hardcover)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "Book",
+      title: "Predicting the Next President",
+      subtitle: "by Allan Lichtman (Paperback)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "Book",
+      title: "Die unregierbare Gesellschaft: Eine Genealogie des autoritären Liberalismus",
+      subtitle: "by Grégoire Chamayou, Michael Halfbrodt (Hardcover)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "Tablet",
+      title: "PocketBook e-Book Reader 'InkPad 3 Pro' 16 GB Memory 19.8 cm (7.8 Inch) E-Ink Carta Display SMARTlight Wi-Fi IPX8 Metallic Grey",
+      subtitle: "",
+      description: "Bücher für unterwegs",
+      fulfilled: false
+    },
+    {
+      icon: "Book",
+      title: "Black Wave: Saudi Arabia, Iran and the Rivalry That Unravelled the Middle East",
+      subtitle: "by Kim Ghattas (Hardcover)",
+      description: "description",
+      fulfilled: false
+    },
   ]
 }
