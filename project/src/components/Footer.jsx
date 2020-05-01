99e1b6c728ee504d03296da674efe33650388be6
maximilian.ungar <maximilian.ungar@klarna.com>
2020-05-02 01:32:04 +0200

    Add content to footer

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "bulma/css/bulma.css";
 
 const Footer = ({ content }) => (
-<footer class="footer  is-hidden-desktop">
-  <div class="has-text-centered">
-      <a className="navbar-item" href={content.paypal_link}>
-        <i className="fab fa-paypal" />
+<footer className="footer  ">
+  <div className="has-text-centered">
+      <a className="is-hidden-desktop" href={content.paypal_link}>
+        <i className="fab fa-paypal" style={{marginRight: 10}}/>
       </a>
-      <a className="navbar-item" href={content.twitter_link}>
+      <a className="is-hidden-desktop" href={content.twitter_link}>
         <i className="fab fa-twitter" />
       </a>
+    <div style={{marginTop: 10}}>Made with 💚</div>
+    <div>Something odd? Create a Pull request <a href="https://github.com/maxrru/wunschliste_einmischen">here</a>.</div>
   </div>
 </footer>
 )
