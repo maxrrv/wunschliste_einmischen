dc085e7e1bbdd5f6589a682a3fb3bce4cfaadbd2
maximilian.ungar <maximilian.ungar@klarna.com>
2020-05-03 16:04:54 +0200

    Add Tech, MicroPhone and Event icons

--- project/src/components/Icons.jsx
+++ project/src/components/Icons.jsx
@@ -8,16 +8,25 @@ const style = {
 const Icons = ({type}) => {
   switch(type) {
     case "Book":
-      return <span style={style} ><i className="fas fa-book" /></span> 
+      return <span style={style} ><i className="fas fa-book" /></span>
       break
     case "GiftCard":
-      return <span style={style} ><i className="fas fa-gift" /></span> 
+      return <span style={style} ><i className="fas fa-gift" /></span>
       break
     case "Tablet":
-      return <span style={style} ><i className="fas fa-tablet-alt" /></span> 
+      return <span style={style} ><i className="fas fa-tablet-alt" /></span>
+      break
+    case "Tech":
+      return <span style={{ ...style, marginLeft: -2 }} ><i className="fas fa-microchip" /></span>
+      break
+    case "MicroPhone":
+      return <span style={{ ...style, marginLeft: 3 }} ><i class="fas fa-microphone-alt" /></span>
+      break
+    case "Event":
+      return <span style={style} ><i class="fas fa-calendar-alt" /></span>
       break
     default: 
-      return <span><i className="fas fa-gift" /></span> 
+      return <span><i className="fas fa-gift" /></span>
       break
   }
 }
