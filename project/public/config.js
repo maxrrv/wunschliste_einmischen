6039a30b7db7ad3a6289748016f2b7ac19425c68
maximilian.ungar <maximilian.ungar@klarna.com>
2020-06-05 19:07:22 +0200

    Update wishlist with current content

--- project/public/config.js
+++ project/public/config.js
@@ -19,43 +19,57 @@ window.websiteContent = {
     },
     {
       icon: "book",
-      title: "The Great Influenza: The Story of the Deadliest Pandemic in History",
-      subtitle: "by John M. Barry (Paperback)",
-      description: "At the height of WWI, history’s most lethal influenza virus erupted in an army camp in Kansas, moved east with American troops, then exploded, killing as many as 100 million people worldwide. It killed more people in twenty-four months than AIDS killed in twenty-four years, more in a year than the Black Death killed in a century. But this was not the Middle Ages, and 1918 marked the first collision of science and epidemic disease. Magisterial in its breadth of perspective and depth of research and now revised to reflect the growing danger of the avian flu, The Great Influenza is ultimately a tale of triumph amid tragedy, which provides us with a precise and sobering model as we confront the epidemics looming on our own horizon. John M. Barry has written a new afterword for this edition that brings us up to speed on the terrible threat of the avian flu and suggest ways in which we might head off another flu pandemic.",
+      title: "Angrynomics",
+      subtitle: "von: Eric Lonergan, Mark Blyth (Taschenbuch)",
+      description: "",
       fulfilled: false
     },
     {
       icon: "book",
-      title: "Very, Very, Very Dreadful: The Influenza Pandemic of 1918",
-      subtitle: "Albert Marrin (Autor), Jim Frangione (Erzähler), Listening Library (Verlag)",
-      description: "In spring of 1918, World War I was underway, and troops at Fort Riley, Kansas, found themselves felled by influenza. By the summer of 1918, the second wave struck as a highly contagious and lethal epidemic and within weeks exploded into a pandemic, an illness that travels rapidly from one continent to another. It would impact the course of the war, and kill many millions more soldiers than warfare itself.",
+      title: "Nichts ist, wie es scheint: Über Verschwörungstheorien (edition suhrkamp)",
+      subtitle: "von: Michael Butter (Taschenbuch)",
+      description: "",
       fulfilled: false
     },
     {
       icon: "book",
-      title: "Simulative Demokratie: Neue Politik nach der postdemokratischen Wende",
-      subtitle: "by Ingolfur Bluehdorn",
-      description: " Sind Proteste wie die der sogenannten »Wutbürger« oder internationale Bewegungen wie Occupy Wall Street ein weiterer Beleg für die Krise der Demokratie? Oder doch eher für ihre Lebendigkeit? Ist »Krise« überhaupt das richtige Wort? Immerhin bezeichnet der Begriff einen vorübergehenden Zustand, aber über Politikverdrossenheit oder gar »Postdemokratie« wird nun schon seit Jahren diskutiert. Ingolfur Blühdorn schlägt eine andere Lesart vor: Wir erleben, so seine These, einen schleichenden Formwandel des Politischen, den Übergang zur »simulativen Demokratie«, in der demokratische Werte, demokratische Verfahren, ja sogar die Idee des demokratischen Souveräns selbst sich gewissermaßen überlebt haben, gleichzeitig aber mehr öffentliche Zustimmung finden denn je - und deshalb als »Simulation« sorgfältig kultiviert werden.",
+      title: "Fake Facts: Wie Verschwörungstheorien unser Denken bestimmen",
+      subtitle: "von: Katharina Nocun, Pia Lamberty (Gebundene Ausgabe)",
+      description: "",
       fulfilled: false
     },
     {
       icon: "book",
-      title: "Ein Mann seiner Klasse",
-      subtitle: "by Christian Baron (Hardcover)",
+      title: "The Wages of Destruction: The Making and Breaking of the Nazi Economy",
+      subtitle: "von: Adam Tooze (Taschenbuch)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "book",
+      title: "Sintflut: Die Neuordnung der Welt 1916-1931",
+      subtitle: "von: Adam Tooze, Norbert Juraschitz (Taschenbuch)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "book",
+      title: "Die politische Theorie des Neoliberalismus (suhrkamp taschenbuch wissenschaft)",
+      subtitle: "von: Thomas Biebricher (Taschenbuch)",
+      description: "",
+      fulfilled: false
+    },
+    {
+      icon: "book",
+      title: "Kapital und Ideologie",
+      subtitle: "von: Thomas Piketty, André Hansen (Gebundene Ausgabe)",
       description: "",
       fulfilled: false
     },
     {
       icon: "book",
       title: "Predicting the Next President",
-      subtitle: "by Allan Lichtman (Paperback)",
-      description: "",
-      fulfilled: false
-    },
-    {
-      icon: "book",
-      title: "Die unregierbare Gesellschaft: Eine Genealogie des autoritären Liberalismus",
-      subtitle: "by Grégoire Chamayou, Michael Halfbrodt (Hardcover)",
+      subtitle: "von: Allan Lichtman (Taschenbuch)",
       description: "",
       fulfilled: false
     },
@@ -67,9 +81,9 @@ window.websiteContent = {
       fulfilled: false
     },
     {
-      icon: "book",
-      title: "Black Wave: Saudi Arabia, Iran and the Rivalry That Unravelled the Middle East",
-      subtitle: "by Kim Ghattas (Hardcover)",
+      icon: "toy",
+      title: "Funko 46763 POP Deluxe: Star Wars-Darth Vader in Meditation Chamber Sammelbares Spielzeug, Mehrfarben",
+      subtitle: "",
       description: "",
       fulfilled: false
     },
